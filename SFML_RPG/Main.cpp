3c025d116fd3334619923924cd107ea47a7c3e06
Thiago Leão <thiago.leao@funkidsapp.com>
2020-05-28 22:07:16 -0300

    Add project files.

---
+++ SFML_RPG/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+#include "SFML/Graphics.hpp"
+
+int main()
+{
+	std::cout << "Hello World" << std::endl;
+	
+	system("pause");
+	return 0;
+}
